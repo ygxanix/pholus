2d5ed25012dd102b7dd02bb04855f21f4b6db54c
ygxanix <88223511+ygxanix@users.noreply.github.com>
2026-03-07 22:17:15 +0530

    Fix: Add Kotlin serialization plugin for build compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.17")
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
     }
     plugins {
         id("com.google.devtools.ksp") version "1.9.22-1.0.17"
+        id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
     }
 }
 
